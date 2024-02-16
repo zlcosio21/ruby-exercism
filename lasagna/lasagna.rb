6eb86605d00a5f38635e4bb0375d05018784fffa
zlcosio21 <sebas60621@gmail.com>
2024-02-15 22:11:13 -0500

    Lasagna

---
+++ lasagna/lasagna.rb
@@ -0,0 +1,16 @@
+class Lasagna
+  PREPARATION_MINUTES_PER_LAYER = 2
+  EXPECTED_MINUTES_IN_OVEN = 40
+
+  def remaining_minutes_in_oven(actual_minutes_in_oven)
+    return EXPECTED_MINUTES_IN_OVEN - actual_minutes_in_oven
+  end
+
+  def preparation_time_in_minutes(layers)
+    return PREPARATION_MINUTES_PER_LAYER * layers
+  end
+
+  def total_time_in_minutes(number_of_layers:, actual_minutes_in_oven:)
+    return (PREPARATION_MINUTES_PER_LAYER * number_of_layers) + actual_minutes_in_oven
+  end
+end
